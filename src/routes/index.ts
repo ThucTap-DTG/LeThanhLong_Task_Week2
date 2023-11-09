c3758469102bbdaa0b48f258cc09a1b4bc3240eb
thanhlong <thanhlongle978@gmail.com>
2023-11-09 16:24:42 +0700

    update path

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,13 @@
 import Student from '../components/student/Student';
-import Create from '../components/student/Create';
-import Update from '../components/student/Update';
-import EditAccount from '../components/student/edit-user';
-import LoginForm from '../components/student/LoginForm';
-import ChangePassword from '../components/student/ChangePassWord';
-import Logout from '../components/student/Logout';
+import EditAccount from '../components/users/edit-user';
+import LoginForm from '../components/users/LoginForm';
+import ChangePassword from '../components/users/ChangePassWord';
+import Logout from '../components/users/Logout';
 import GetSubject from '../components/subject/GetSubject';
 
 const publicRoutes = [
     {path: '/', component: LoginForm, layout: null},
-    {path: '/create', component: Create},
     {path: '/student', component: Student},
-    {path: '/update', component: Update},
     {path: '/edit-user', component: EditAccount},
     {path: '/changepassword', component: ChangePassword},
     {path: '/logout', component: Logout},
